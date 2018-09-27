2bb04ac9b05e18edff5fb4c081368f2fe1dea8e1
Dmatrix <Dimasebakovgoo@gmail.com>
2018-09-27 16:35:21 +0300

    Update request_handler.py

--- src/request_handler.py
+++ src/request_handler.py
@@ -5,9 +5,9 @@ def handle_request(data):
     if 'type' not in data.keys():
         return 'not vk'
     if data['type'] == 'confirmation':
-        return os.environ.get("VKCONFIRMATION_TOKEN")
+        return os.environ.get('VKCONFIRMATION_TOKEN')
     elif data['type'] == 'message_new' or data['type'] == 'message_edit':
-        return "ok"
+        return 'ok'
     elif data['type'] == 'message_reply' or data['type'] == 'message_typing_state':
         return 'ok'
     return 'unknown'
