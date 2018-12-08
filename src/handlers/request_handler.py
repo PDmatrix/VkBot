f3d8f90351920c4a48c148cd2245ad2a2c466439
Dmatrix <dimasebakovgoo@gmail.com>
2018-12-08 20:53:06 +0300

    Updating log message and code refactor

--- src/handlers/request_handler.py
+++ src/handlers/request_handler.py
@@ -11,19 +11,21 @@ def handle_request(data):
     if data['type'] == 'confirmation':
         return os.environ.get('VK_CONFIRMATION_TOKEN')
     elif data['type'] == 'message_new' or data['type'] == 'message_edit':
-        logger.info("Starting to process request...", request=data)
+        logger.info("Processing request...", request=data)
         try:
             start_time = time.time()
             message_handler.handle_message(data['object'])
-            logger.info("Time elapsed: {elapsed}s", elapsed=f"{time.time() - start_time:.2f}")
+            logger.info(
+                "Time elapsed: {elapsed}s",
+                elapsed=f"{time.time() - start_time:.2f}")
         except Exception:
-            logger.error("Exception occurred while handling request: {request}",
-                         exception=traceback.format_exc(),
-                         request=data)
+            logger.error(
+                "Exception occurred while handling request: {request}",
+                exception=traceback.format_exc(),
+                request=data)
             return 'error'
         return 'ok'
     elif data['type'] == 'message_reply':
         logger.info("Responding...", response=data)
         return 'ok'
     return 'unknown'
-
