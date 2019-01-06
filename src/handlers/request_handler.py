e0e6f9f26334f2e2ffed3a5262188f734d037391
Dmatrix <dimasebakovgoo@gmail.com>
2019-01-06 22:52:01 +0300

    Less verbose logging

--- src/handlers/request_handler.py
+++ src/handlers/request_handler.py
@@ -12,7 +12,7 @@ def handle_request(data):
     if data['type'] == 'confirmation':
         return os.environ.get('VK_CONFIRMATION_TOKEN')
     elif data['type'] == 'message_new' or data['type'] == 'message_edit':
-        logger.info("Processing request...", request=data)
+        logger.info("Processing request...", request=data['object'])
         try:
             start_time = time.time()
             message_handler.handle_message(data['object'])
@@ -28,6 +28,5 @@ def handle_request(data):
                        "Возникла ошибка. Пожалуйста, попробуйте снова.")
         return 'ok'
     elif data['type'] == 'message_reply':
-        logger.info("Responding...", response=data)
         return 'ok'
     return 'unknown'
